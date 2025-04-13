5ce9c298d70f74bb2e542711eed3f634265e0c00
Your Name <tranminhthuong08082003@82003gmailg.cogmailm>
2025-04-13 17:14:13 +0700

    Xóa tất cả thư mục

--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-module HeThongQuanLyLopHocOnline {
-	requires java.desktop; // Cần cho Java Swing
-	requires java.sql; // Cần cho JDBC (nếu bạn dùng SQL)
-	requires org.apache.poi.poi;
-	requires org.apache.poi.ooxml;
-	requires jcalendar;
-	requires com.github.lgooddatepicker; // Thư viện JCalendar
-}
