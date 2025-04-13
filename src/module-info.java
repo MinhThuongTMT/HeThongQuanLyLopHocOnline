989d05d12f183917d23480f9928ab38231d39568
Your Name <tranminhthuong08082003@82003gmailg.cogmailm>
2025-04-13 17:18:19 +0700

    update all code new

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module HeThongQuanLyLopHocOnline {
+	requires java.desktop; // Cần cho Java Swing
+	requires java.sql; // Cần cho JDBC (nếu bạn dùng SQL)
+	requires org.apache.poi.poi;
+	requires org.apache.poi.ooxml;
+	requires jcalendar;
+	requires com.github.lgooddatepicker; // Thư viện JCalendar
+}
