2daa7a550de44aa47b1455b3aa7b9e1402a51006
Tran Minh Thuong <161148515+MinhThuongTMT@users.noreply.github.com>
2025-05-26 17:24:28 +0700

    Update QuanLyGiangVien.java
    Update account database new

--- HeThongQuanLyLopHocOnline/src/QuanLyLopHoc/QuanLyGiangVien.java
+++ HeThongQuanLyLopHocOnline/src/QuanLyLopHoc/QuanLyGiangVien.java
@@ -30,9 +30,9 @@ public class QuanLyGiangVien extends JPanel {
 	private JPanel currentPanel;
 
 	// Thông tin kết nối database
-	private static final String DB_URL = "jdbc:postgresql://aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres?sslmode=require&pgbouncer=true";
-	private static final String DB_USERNAME = "postgres.vpehkzjmzpcskfzjjyql";
-	private static final String DB_PASSWORD = "MinhThuong0808";
+        private static final String DB_URL = "jdbc:postgresql://aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres?sslmode=require&pgbouncer=true";
+        private static final String DB_USERNAME = "postgres.vrnqdslteaehazpwkipz";
+        private static final String DB_PASSWORD = "MinhThuong0808";
 
 	public QuanLyGiangVien() {
 		setBackground(new Color(0, 0, 121));
