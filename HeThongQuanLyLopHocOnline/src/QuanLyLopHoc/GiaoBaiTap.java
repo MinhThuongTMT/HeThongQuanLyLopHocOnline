d6875ad1c92c57567fdef5514de5f64471bf8745
Tran Minh Thuong <161148515+MinhThuongTMT@users.noreply.github.com>
2025-05-26 17:23:14 +0700

    Update GiaoBaiTap.java
    Update account database new

--- HeThongQuanLyLopHocOnline/src/QuanLyLopHoc/GiaoBaiTap.java
+++ HeThongQuanLyLopHocOnline/src/QuanLyLopHoc/GiaoBaiTap.java
@@ -35,7 +35,7 @@ public class GiaoBaiTap extends JFrame {
 
     // Thông tin kết nối database
     private static final String DB_URL = "jdbc:postgresql://aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres?sslmode=require&pgbouncer=true";
-    private static final String DB_USERNAME = "postgres.vpehkzjmzpcskfzjjyql";
+    private static final String DB_USERNAME = "postgres.vrnqdslteaehazpwkipz";
     private static final String DB_PASSWORD = "MinhThuong0808";
 
     // Constructor mặc định
