75d4eb74d10f4ca321bd722bcc7be72963106a4f
Tran Minh Thuong <161148515+MinhThuongTMT@users.noreply.github.com>
2025-05-26 17:20:14 +0700

    Update BaiTap.java
    Updata account database new

--- HeThongQuanLyLopHocOnline/src/QuanLyLopHoc/BaiTap.java
+++ HeThongQuanLyLopHocOnline/src/QuanLyLopHoc/BaiTap.java
@@ -36,10 +36,10 @@ public class BaiTap extends JFrame {
     private JComboBox<String> tieuDeComboBox;
     private JTextArea noiDungTextArea;
 
-    // Thông tin kết nối database
-    private static final String DB_URL = "jdbc:postgresql://aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres?sslmode=require&pgbouncer=true";
-    private static final String DB_USERNAME = "postgres.vpehkzjmzpcskfzjjyql";
-    private static final String DB_PASSWORD = "MinhThuong0808";
+	// Thông tin kết nối database
+	private static final String DB_URL = "jdbc:postgresql://aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres?sslmode=require&pgbouncer=true";
+	private static final String DB_USERNAME = "postgres.vrnqdslteaehazpwkipz";
+	private static final String DB_PASSWORD = "MinhThuong0808";
 
     public BaiTap(String hoTen, String mssv) {
         // Khởi tạo cơ sở dữ liệu và giao diện
