74f86e34957da1caa6c86eeb2198e0c5c8e8cc3f
Tran Minh Thuong <161148515+MinhThuongTMT@users.noreply.github.com>
2025-05-26 17:26:30 +0700

    Update ThongTinGiangVien.java
    Update account database new

--- HeThongQuanLyLopHocOnline/src/QuanLyLopHoc/ThongTinGiangVien.java
+++ HeThongQuanLyLopHocOnline/src/QuanLyLopHoc/ThongTinGiangVien.java
@@ -31,7 +31,7 @@ public class ThongTinGiangVien extends JFrame {
 
     // Thông tin kết nối database
     private static final String DB_URL = "jdbc:postgresql://aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres?sslmode=require&pgbouncer=true";
-    private static final String DB_USERNAME = "postgres.vpehkzjmzpcskfzjjyql";
+    private static final String DB_USERNAME = "postgres.vrnqdslteaehazpwkipz";
     private static final String DB_PASSWORD = "MinhThuong0808";
 
     // Regex cho email và số điện thoại
